103a0c9590973d2da4a51b6b080f4783979ca2f9
Andre Oliveira <andre.fco33@gmail.com>
2017-03-30 16:22:04 +0100

    libgdx project started

---
+++ desktop/src/org/academiadecodigo/vim4cache/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.academiadecodigo.vim4cache.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.academiadecodigo.vim4cache.CaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CaGame(), config);
+	}
+}
