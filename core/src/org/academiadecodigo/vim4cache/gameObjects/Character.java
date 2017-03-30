7b603a90cf56cb654e91b138ed81ddc358824327
Fred Akex <frederico.alexandre94@hotmail.com>
2017-03-30 18:04:07 +0100

    Begining Character

--- core/src/org/academiadecodigo/vim4cache/gameObjects/Character.java
+++ core/src/org/academiadecodigo/vim4cache/gameObjects/Character.java
@@ -1,32 +1,44 @@
 package org.academiadecodigo.vim4cache.gameObjects;
 
-import com.badlogic.gdx.math.Vector2;
-
-import java.awt.*;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.physics.box2d.*;
 
 /**
  * Created by codecadet on 30/03/17.
  */
-public class Character {
-    private Vector2 position;
-    private Vector2 velocity;
-    private int width;
-    private int height;
-    private Rectangle characterRectangle;
+public class Character extends Sprite{
+    private int health;
+    private World world;
+    private Screen screen;
+    public Body b2body;
 
-    public Character(int x, int y, int width, int height){
-        this.width = width;
-        this.height = height;
-        this.position = new Vector2(x, y);
-        this.velocity = new Vector2(0, 0);
-        this.characterRectangle = new Rectangle(x, y, width, height);
+
+    public Character(World world, Screen screen) {
+        this.world = world;
+        this.screen = screen;
+        defineCharacter();
     }
 
     public void update(float delta) {
-
-
-
-
-
+        setPosition(b2body.getPosition().x-getWidth()/2 , b2body.getPosition().y - getHeight()/2);
     }
+
+    public void defineCharacter() {
+        this.health = 100;
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set(200, 600);
+        b2body = world.createBody(bodyDef);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        PolygonShape polygonShape = new PolygonShape();
+        polygonShape.setAsBox(50, 25);
+
+
+        fixtureDef.shape = polygonShape;
+        b2body.createFixture(fixtureDef);
+    }
+
+
+
 }
