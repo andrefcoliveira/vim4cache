30e16b874056be57381e90434d109d3027dc4722
Tiago Duarte <tiagotosduarte@gmail.com>
2017-03-31 08:10:58 +0100

    simple endScreen

---
+++ core/src/org/academiadecodigo/vim4cache/screens/EndScreen.java
@@ -0,0 +1,79 @@
+package org.academiadecodigo.vim4cache.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import org.academiadecodigo.vim4cache.CaGame;
+import org.academiadecodigo.vim4cache.util.VariablesUtil;
+
+
+/**
+ * Created by codecadet on 31/03/17.
+ */
+public class EndScreen implements Screen {
+    private Viewport viewPort;
+    private Stage stage;
+    private CaGame caGame;
+
+    public EndScreen(CaGame caGame) {
+        this.caGame = caGame;
+        viewPort = new FitViewport(VariablesUtil.V_WIDTH, VariablesUtil.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewPort, caGame.batch);
+
+        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+
+        Label gameOverLabel = new Label("Game Over!", font);
+        table.add(gameOverLabel).expandX();
+
+        stage.addActor(table);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
